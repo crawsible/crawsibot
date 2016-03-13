dbd5f4f7eec598b4aa77b19d44b509a91ab53747
Colin Jackson <colin.s.jackson90@gmail.com>
2016-03-13 03:11:30 -0400

    Add join message integration test

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -22,8 +22,8 @@ var _ = Describe("Integration", func() {
 		command := exec.Command(
 			crawsibotPath,
 			"-a", "localhost:3000",
-			"-n", "some-username",
-			"-p", "some-password",
+			"-n", "someusername",
+			"-p", "somepassword",
 			"-c", "somechannel",
 		)
 
@@ -44,8 +44,8 @@ var _ = Describe("Integration", func() {
 	})
 
 	It("validates with the server using the specified credentials", func() {
-		Eventually(reqCh).Should(Receive(Equal("PASS some-password\r\n")))
-		Eventually(reqCh).Should(Receive(Equal("NICK some-username\r\n")))
+		Eventually(reqCh).Should(Receive(Equal("PASS somepassword\r\n")))
+		Eventually(reqCh).Should(Receive(Equal("NICK someusername\r\n")))
 	})
 
 	It("registers IRCv3 capabilities with the server", func() {
@@ -59,14 +59,20 @@ var _ = Describe("Integration", func() {
 	})
 
 	It("joins the specified channel", func() {
-		Eventually(reqCh).Should(Receive(Equal("NICK some-username\r\n")))
+		Eventually(reqCh).Should(Receive(Equal("CAP REQ :twitch.tv/membership\r\n")))
 		Consistently(reqCh).ShouldNot(Receive(Equal("JOIN #somechannel\r\n")))
 
 		resCh <- ":tmi.twitch.tv 376 crawsibot :>\r\n"
 		Eventually(reqCh).Should(Receive(Equal("JOIN #somechannel\r\n")))
 	})
 
-	XIt("Announces its arrival", func() {
+	It("Announces its arrival", func() {
+		Eventually(reqCh).Should(Receive(Equal("CAP REQ :twitch.tv/membership\r\n")))
+		resCh <- ":tmi.twitch.tv 376 crawsibot :>\r\n"
+		Eventually(reqCh).Should(Receive(Equal("JOIN #somechannel\r\n")))
+
+		resCh <- ":someusername.tmi.twitch.tv 353 someusername = #somechannel :someusername\r\n"
+		resCh <- ":someusername.tmi.twitch.tv 366 someusername #somechannel :End of /NAMES list\r\n"
 		Eventually(reqCh).Should(Receive(Equal("PRIVMSG #somechannel :COME WITH ME IF YOU WANT TO LIVE.")))
 	})
 })
